411a61c2b25afbf476bf70fdb8f28813fe23a12b
MalleshGit <hpmallesh88@gmail.com>
2019-02-19 23:39:38 +0530

    Signed-off-by: MalleshGit <hpmallesh88@gmail.com>

---
+++ DMS.Constaints/DataBaseConstat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMS.Constat
+{
+    public static class DdataLayerConstat
+    {
+        /*DB connection*/
+        public static string ConnectionString = "CommunityConnectionString";
+
+
+        /*store procedure*/
+        public static string DMS_sp_adduser = "DMS_sp_adduser";
+
+
+        /*parameters*/
+
+        public static string usrname = "usrname";
+        public static string email = "email";
+        public static string pwd = "pwd";
+        public static string error_code = "error_code";
+        public static string error_message = "error_message";
+       
+    }
+}
